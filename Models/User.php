60f5a1ea4a81706412bb543ef916467e45510081
lina <kamadlina12gmail.com>
2026-04-22 22:35:57 +0100

    j'ai commencer user.php avec la validation du email + password(encore)

---
+++ Models/User.php
@@ -0,0 +1,43 @@
+<?php
+
+    class User{
+        private $db;
+        public function __construct($cnx)
+        {
+            $this->db = $cnx;
+        }
+
+        //Valdation email:
+
+        public function validEmail($email){
+            $patternEmail = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
+            if(!preg_match($patternEmail, $email)){
+                return ['valid'=>false, 'message'=>"Format email invalide!"];
+            }
+            return['valid'=>true];
+        }
+        //si email deja kayn:
+        public function emailExist($email){
+            $stmt = $this->db->prepare("SELECT id_user FROM user WHERE Gmail = ?");
+            $stmt->execute([$email]);
+            return $stmt->rowCount() > 0; //rowCount: return le nombre des lignes trouvées/affectés par la requete sql
+
+        }
+
+        //-------------------------------------------
+
+        //validation password
+
+        public function validPass($password){
+            $errors = [];
+            if(strlen($password) < 8){
+                $errors[] = "Minimum 8 caractères!!";
+            }
+            if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{};\':"\\|,.<>\/?]).+$/', $password)) {
+                $errors[] = '❌ Mot de passe doit contenir : minuscule, majuscule, chiffre et caractère spécial';
+            }
+        }
+        
+    }
+
+?>
